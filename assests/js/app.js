a09633050138756e82c2f49b34c9a823523f6812
root <gauchanneelam@gmail.com>
2016-11-18 12:27:40 +0545

    sidebar added

--- assests/js/app.js
+++ assests/js/app.js
@@ -1,6 +1,24 @@
-$(document).ready(function() {
-    $("#logout").click(function(){
-        window.location.href ="login.html";
-      }
+$(document).ready(function(){
+  var backendUrl = "http://192.99.172.172:8000/api";
+  //login submit button click
+  $("#loginForm").click(function(){             // click submit button;
+      var username= $("#username").val().trim();
+      var password = $("#password").val().trim();
+       var data = {
+            "username":username,
+            "password":password
+        };
+        // debugger;
+    $.post(backendUrl+ '/authenticate', data).done(function(response) {
+          //user authrnticated
+          console.log("response", response);
+            window.location.href ="index.html";
+         });
+
     });
-  });
+  });// document close
+
+
+  $(function () {
+     $('#menu').metisMenu();
+   });
