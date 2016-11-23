75f43d951a26676c7a59f8bc420f572ce6937d26
neelam <gauchanneelam@gmail.com>
2016-11-23 10:51:57 +0545

    edit page updated

--- assests/js/app_back.js
+++ assests/js/app_back.js
@@ -1,5 +1,6 @@
-var backendUrl = "http://172.16.10.127:8000/api";
+  var backendUrl = "http://172.16.10.127:8000/api";
 $(document).ready(function(){
+
   //login submit button click
   $("#loginForm").click(function(){             // click submit button;
       var username= $("#username").val().trim();
@@ -15,7 +16,7 @@ $(document).ready(function(){
             window.location.href ="index.html";
          });
        });
-    $.get(backendUrl+ '/product').done(function(response){
+    $.get(backendUrl+ '/product').done(function(response){  //post
               console.log("response",response);
               response.forEach(function(product){
                  $("<tr><td>"+product.name +"</td>" +"<td>"+product.retailPrice +"</td>" +"<td>"+product.description +"</td></tr>").appendTo("tbody");
@@ -23,64 +24,24 @@ $(document).ready(function(){
               // document.getElementById('name').innerHTML = product.name;
               //   document.getElementById('retail price').innerHTML = product.retailPrice;
               //  $("td").append(product.name);
-            });
-          });
-        });
-  //edit submit button
-  $("#editForm").click(function(){
-      var userid = localStorage.getItem("id");            // click submit button;
-      var username= $("#username").val().trim();
-          password= $("#password").val().trim();
-          fullname= $("#fullname").val().trim();
-          mobile=   $("#mobile").val().trim();
-          birthday= $("#dob").val().trim();
-          gender=   $("#password").val().trim();
-
-       var data = {
-            "username":username,
-            "password":password,
-            "fullName":fullname,
-            "mobileNumber":mobile,
-            "birthDay":birthday,
-            "gender":gender
-        };
-        $.ajax({
-           url:backendUrl+ '/users/'+ userid,
-           type: 'PUT',
-           data:data,
-            }).done(function(response){
-              //  $("<tr><td>"+response.fullname +"</td>"+"<td>"+response.mobile+"</td></tr>").appendTo("tbody");
-              console.log("response",response);
-                window.location.href ="userlist.html";
-              });
-        });
-
+       });
+  });
 //Edit
   $(document).on('click','.edit',function(){
-      var userid = ($(this).attr('data-userid'));
-      localStorage.setItem("id", userid);
-    });
-//delete
-     $(document).on('click','.delete',function(){
-     var userid = ($(this).attr('data-userid'));
-     console.log('id',userid);
+    var userid = ($(this).attr('data-userid'));
     $(this).parent().parent().remove();
+  });
+//delete
+  $(document).on('click','.delete',function(){
+    var userid = ($(this).attr('data-userid'));
+    console.log('id',userid);
 
-   //$.delete(backendUrl+ '/users/'+userid, {}).done(function(response){
-         //console.log("response", response);
-        // $(this).parent().parent().remove();
-         //});
-       $.ajax({
-          url:backendUrl+ '/users/'+ userid,
-          type: 'DELETE',
-      }).done(function(response){
-        var msg =response.message;
-        $("p").append(msg);
+   $.delete(backendUrl+'/users/'+userid, data).done(function(response){
+         $(this).parent().parent().remove();
+         });
+       });
 
-      });
-    });
-
-//leftside menu
+//left side menu
 $(function () {
    $('#menu').metisMenu();
  });
@@ -104,7 +65,7 @@ $(document).ready(function(){
         localStorage.setItem("username", username);
         localStorage.setItem("password", password);
         $("p").append(msg);
-        window.location.href ="userlist.html";
+        window.location.href ="users.html";
         });
       });
               // .get(backendUrl+ '/users').done(function(response){
@@ -131,24 +92,7 @@ $(document).ready(function(){
        $.get(backendUrl+ '/users').done(function(response){  //post
                  console.log("response",response);
                  response.forEach(function(user){
-                    $("<tr><td>"+user.username +"</td>"+"<td>"+user.password +"</td>"+"<td><button class='edit' data-userid='"+user._id+"'><a href='editpage.html'>Edit</a></button></td>" +"<td><button class='delete' data-userid="+user._id+">Delete</button></td></tr>").appendTo("tbody");
+                    $("<tr><td>"+user.username +"</td>"+"<td>"+user.password +"</td>" +"<td><button class='edit' data-userid='"+user._id+"'> Edit</button></td>" +"<td><button class='delete' data-userid="+user._id+">Delete</button></td></tr>").appendTo("tbody");
                   });
               });
-      }
-
-      function editUser(){
-        var userid = localStorage.getItem("id");
-        console.log("userid", userid);
-        $.get(backendUrl+ '/users/'+userid).done(function(response){
-                  console.log("response",response);
-                  document.getElementById('username').value= response.username;
-                  document.getElementById('password').value= response.password;
-                });
-              }
-         // console.log("username" , username, "password", password);
-           // $.ajax({
-           //     url: backendUrl +"/users",
-           //     type: "GET",
-           //     beforeSend: function(xhr){xhr.setRequestHeader("Authorization", "Basic " +"admin" + ":" + "admin");},
-           //     success: function() { }
-           //  });
+            }
